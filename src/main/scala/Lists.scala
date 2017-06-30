c382e4c8def4922f69e376783a4d47ed7bd1ac7e
Ivan Obeso <ivan.obeso1@gmail.com>
2017-06-30 13:36:28 +0100

    2 more solutions added (easy peasy)

--- src/main/scala/Lists.scala
+++ src/main/scala/Lists.scala
@@ -1,8 +1,31 @@
 object Lists {
 
+  /**
+    * P01 (*) Find the last element of a list.
+    */
   def last[A](l: List[A]): Option[A] = l match {
     case x :: Nil => Some(x)
     case x :: xs => last(xs)
+    case Nil => None
+  }
+
+  /**
+    * P02 (*) Find the last but one element of a list.
+    */
+  def penultimate[A](l: List[A]): Option[A] = l match {
+    case x :: y :: Nil => Some(x)
+    case x :: xs => penultimate(xs)
+    case Nil => None
+  }
+
+  /**
+    * P03 (*) Find the Kth element of a list.
+    */
+  def nth[A](i: Int, l: List[A]): Option[A] = l match {
+    case x :: xs => {
+      if(i == 0) Some(x)
+      else nth(i-1, xs)
+    }
     case Nil => None
   }
 }
--- src/test/scala/ListsSuite.scala
+++ src/test/scala/ListsSuite.scala
@@ -12,4 +12,16 @@ class ListsSuite extends FunSuite {
     assert(Lists.last(List()) == None)
   }
 
+  test("P02 - Find the last but one element of a list") {
+    assert(Lists.penultimate(List(1,2,3)) == Some(2))
+    assert(Lists.penultimate(List(1,2)) == Some(1))
+    assert(Lists.penultimate(List()) == None)
+  }
+
+  test("P03 - Find the Kth element of a list") {
+    assert(Lists.nth(0,List(1,2,3)) == Some(1))
+    assert(Lists.nth(1,List(1,2,3)) == Some(2))
+    assert(Lists.nth(2,List(1,2,3)) == Some(3))
+    assert(Lists.nth(3,List(1,2,3)) == None)
+  }
 }
