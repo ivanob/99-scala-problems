d20b7baa4d86923464d3efea52354596853ae244
Ivan Obeso <ivan.obeso1@gmail.com>
2017-09-10 18:43:07 +0100

    added a couple of solutions

--- src/main/scala/S99Int.scala
+++ src/main/scala/S99Int.scala
@@ -2,6 +2,9 @@
 class S99Int(val i: Int){
   import S99Int._
 
+  /**
+    * P31 (**) Determine whether a given integer number is prime.
+    */
   def isPrime:Boolean = {
     if(i>0 && i<=3) true
     else if(i<=0) false
@@ -10,8 +13,24 @@ class S99Int(val i: Int){
       !(2 to root).toList.map(i % _).exists(_ == 0)
     }
   }
+
+  /**
+    * P33 (*) Determine whether two positive integer numbers are coprime.
+    */
+  def isCoprimeTo(b: Int):Boolean = {
+    gcd(i,b)==1
+  }
 }
 
 object S99Int{
   implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
+
+  /**
+  P32 (**) Determine the greatest common divisor of two positive integer numbers.
+    https://en.wikipedia.org/wiki/Euclidean_algorithm
+    **/
+  def gcd(a:Int, b:Int): Int = {
+    if(b==0) a
+    else gcd(b, a % b)
+  }
 }
--- src/test/scala/ArithmeticSuit.scala
+++ src/test/scala/ArithmeticSuit.scala
@@ -13,4 +13,15 @@ class ArithmeticSuit extends FunSuite {
     (1 to 30).map(x => assert((x.isPrime==true && primeNumbers.contains(x)) ||
       (x.isPrime==false && !primeNumbers.contains(x))))
   }
+
+  test("P32 - Determine the greatest common divisor of two positive integer numbers.") {
+    assert(gcd(36, 63)==9)
+    assert(gcd(1071, 462)==21)
+  }
+
+  test("P33 - Determine whether two positive integer numbers are coprime.") {
+    assert(35.isCoprimeTo(64)==true)
+    assert(36.isCoprimeTo(63)==false)
+  }
+
 }
